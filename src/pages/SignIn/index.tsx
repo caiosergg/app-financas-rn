4183f48bf589dd09e30b18b70713a54903191010
caiosergg <caiosergio123@gmail.com>
2025-03-01 16:30:55 -0300

    Initial commit

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const SignIn: React.FC = () => {
+  return (
+    <View>
+      <Text>TELA LOGIN</Text>
+    </View>
+  );
+};
+
+export default SignIn;
